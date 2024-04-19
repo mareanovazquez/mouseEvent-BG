b1a5f484dae99129b55ef09c1cd2c295b89c9074
Mariano Vázquez <mareanovazquez@protonmail.com>
2024-04-19 10:18:35 -0300

    cambio en la funcionalidad y la vista

--- js/animateBG.js
+++ js/animateBG.js
@@ -1,27 +1,40 @@
-let pg = window.innerWidth;
-let py = window.innerHeight;
 
-let blok_sayisi = 60;
 
-let bg = pg / blok_sayisi;
+/*Obtener las dimensiones del contenedor*/
+let contenedor = document.querySelector('.contenedor');
+let contenedorWidth = contenedor.offsetWidth;
+let contenedorHeight = contenedor.offsetHeight;
 
-for (let i = 1; i <= 20000; i++) {
+/*es el número de elementos de lista (<li>) que se crearán */
+let bloqueCreate = 60;
+
+/*bg calcula el ancho de cada elemento <li> dividiendo el ancho de la ventana (pg) por el número de elementos. */
+let bg = contenedorWidth / bloqueCreate;
+
+/*Se crean 2000 elementos
+Cada uno tiene un id único id="bloque1, bloque2..."
+Cada elemento tiene una clase bloque
+Se agregan como hijos de  bgmouseE  */
+for (let i = 1; i <= 1000; i++) {
     let li = document.createElement("li");
-    li.id = "blok" + i;
-    li.className = "blok";
+    li.id = "bloque" + i;
+    li.className = "bloque";
     li.style.width = bg + "px";
     li.style.height = bg + "px";
-    document.getElementById("kutular").appendChild(li);
+    
+    document.getElementById("bgmouseE").appendChild(li);
+
 }
 
-let kutularLi = document.querySelectorAll("ul#kutular li");
 
-kutularLi.forEach(function (element) {
-    element.addEventListener("mouseover", function () {
-        let index = Array.from(kutularLi).indexOf(element);
-        element.style.background = "#000";
+let bgmouseELi = document.querySelectorAll("ul#bgmouseE li");
+
+bgmouseELi.forEach(function (e) {
+    e.addEventListener("mouseover", function () {
+        let index = Array.from(bgmouseELi).indexOf(e);
+        e.style.background = "#E6E6E6";
         setTimeout(function () {
-            kutularLi[index].style.background = "#ffa900";
-        }, 6000);
+            bgmouseELi[index].style.background = "#CFCFCF";
+        }, 2000);
     });
 });
